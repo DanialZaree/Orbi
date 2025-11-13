37bfc68cee811d9f3c54b73fe1fa4295db65c324
Danial Zaree <danialzaree0@gmail.com>
2025-11-13 22:49:27 +0330

    responsive and added bg dark

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,14 +4,10 @@ const apiClient = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
-// --- THIS IS THE FIX (Part 1) ---
-// This interceptor runs before every request.
 apiClient.interceptors.request.use(
   (config) => {
-    // Read the token from localStorage on every request
     const token = localStorage.getItem("authToken");
     if (token) {
-      // If the token exists, add it to the header
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
